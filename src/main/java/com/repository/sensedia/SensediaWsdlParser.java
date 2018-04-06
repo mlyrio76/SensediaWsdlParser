38779805337761df1bd6061a32a5299b2aba8f90
mlyrio <mlyrio@DA000347>
2018-04-06 09:41:47 -0300

    Versão Beta

--- src/main/java/com/repository/sensedia/SensediaWsdlParser.java
+++ src/main/java/com/repository/sensedia/SensediaWsdlParser.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WsdlToTabularFileApplication {
+public class SensediaWsdlParser {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WsdlToTabularFileApplication.class, args);
+		SpringApplication.run(SensediaWsdlParser.class, args);
 	}
 }
--- src/test/java/com/repository/sensedia/SensediaWsdlParserTests.java
+++ src/test/java/com/repository/sensedia/SensediaWsdlParserTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WsdlToTabularFileApplicationTests {
+public class SensediaWsdlParserTests {
 
 	@Test
 	public void contextLoads() {
